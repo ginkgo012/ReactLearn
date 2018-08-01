f8a595b35a7879f3c65f69ae8128788f9937878b
Mingjia Shi <shimj9312@gmail.com>
2018-08-01 22:20:03 +0200

    fix bug

--- my-app/src/components/contact.js
+++ my-app/src/components/contact.js
@@ -10,7 +10,7 @@ class Contact extends Component{
                 My name is Mingjia and I am a developer. </p>
                 <p>I am a newly graduate from Unviersity of Copenhagen in Denmark. My major is geo-informatics, which connects closely to IT. </p>
                 <p>I worked in an interesting start-up for half a year. I love coding, so I build this website by my own to display some coding toys.</p>
-                <p>If you are interested in having a talk with me, you are welcome to <a href="mailto:drm314@alumni.ku.dk">send a mail</a> to me. Here is my <a href="https://www.linkedin.com/in/shi-mingjia-858234104/">Linkedin</a>, you can also leave a message here.            </p>
+                <p>If you are interested in having a talk with me, you are welcome to <a href="mailto:drm314@alumni.ku.dk">send a mail</a> to me. </p>
             </div>
 
         )
